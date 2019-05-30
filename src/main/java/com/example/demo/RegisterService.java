0b58a2bbf1e04ad9cc3137bbd74c6b0033d2b6b0
Kubarant <kiba50@gazeta.pl>
2019-05-30 21:52:27 +0200

    extract logic from registerController to dedicated service

---
+++ src/main/java/com/example/demo/RegisterService.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import com.example.demo.model.User;
+import com.example.demo.model.UserRegisterDTO;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class RegisterService {
+
+    private final UserRepository userRepository;
+    private final ActivationEmailSender emailService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public RegisterService(UserRepository userRepository, ActivationEmailSender emailService, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.emailService = emailService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registerWithoutEmailActivation(UserRegisterDTO userRegisterDTO){
+        User user = prepareNewUser(userRegisterDTO);
+        user.setActivated(true);
+        userRepository.save(user);
+    }
+
+    public void register( UserRegisterDTO userRegisterDTO){
+        User user = prepareNewUser(userRegisterDTO);
+        emailService.sendActivationMessage(user);
+        userRepository.save(user);
+
+    }
+
+    private User prepareNewUser(UserRegisterDTO userRegisterDTO) {
+        LocalDate now = LocalDate.now();
+        String userToken = MD5Util.md5(now.toString() + userRegisterDTO.getUsername());
+        String passwordHash = passwordEncoder.encode(userRegisterDTO.getPassword());
+        return new User(userRegisterDTO.getUsername(),passwordHash, now, userToken, userRegisterDTO.getEmail());
+    }
+
+
+
+
+}
--- src/main/java/com/example/demo/controllers/RegisterController.java
+++ src/main/java/com/example/demo/controllers/RegisterController.java
@@ -1,47 +1,32 @@
 package com.example.demo.controllers;
 
-import java.time.LocalDate;
-
+import com.example.demo.RegisterService;
+import com.example.demo.model.UserRegisterDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.example.demo.EmailService;
-import com.example.demo.MD5Util;
-import com.example.demo.model.User;
-import com.example.demo.repositories.UserRepository;
-
 @Controller
 public class RegisterController {
 
-	@Autowired
-	UserRepository userRepository;
-	@Autowired
-	EmailService emailService;
-	@Autowired
-	PasswordEncoder passwordEncoder;
+    private final RegisterService registerService;
 
-	@GetMapping("/register")
-	public String register() {
+    @Autowired
+    public RegisterController(RegisterService registerService) {
+        this.registerService = registerService;
+    }
 
-		return "register";
-	}
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
 
-	@PostMapping("/register")
-	public String registering(User user) {
-		LocalDate now = LocalDate.now();
-
-		user.setRegisterDate(now);
-		user.setPassword(passwordEncoder.encode(user.getPassword()));
-		user.setActivated(true);
-		user.setToken(MD5Util.md5(now.toString() + user.getUsername()));
-		userRepository.save(user);
-		//emailService.sendActivationMessage(user);
-		return "login";
-	}
-
+    @PostMapping("/register")
+    public String registering(UserRegisterDTO userRegisterDTO) {
+        registerService.register(userRegisterDTO);
+        return "login";
+    }
 
 
 }
