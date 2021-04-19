41abd1cb6fa06eaeffb5e8775b474c5437569c21
Kubarant <kiba50@gazeta.pl>
2021-04-19 12:35:41 +0200

    add account activation controller

---
+++ src/main/java/com/example/demo/register/ActivationController.java
@@ -0,0 +1,26 @@
+package com.example.demo.register;
+
+import com.example.demo.model.User;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/activation/")
+public class ActivationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/{token}")
+    public String register(@PathVariable String token) {
+        User user = userRepository.findByToken(token);
+        user.setActivated(true);
+        userRepository.save(user);
+        return "login";
+    }
+
+}
